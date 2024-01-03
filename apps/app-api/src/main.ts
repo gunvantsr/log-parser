d0c84706a94968df764b2bbde76fcee95dd1b408
Gunvant Sarpate <gunvantsr@gmail.com>
2024-01-03 17:23:19 +0530

    refact: added config lib

--- apps/app-api/src/main.ts
+++ apps/app-api/src/main.ts
@@ -5,9 +5,9 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+import { corsConfig } from '@log-parser/server-core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,13 +15,7 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
 
-  const corsOptions: CorsOptions = {
-    origin: 'http://localhost:4200', // Adjust this to your client's origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  };
-
-  app.enableCors(corsOptions);
+  app.enableCors(corsConfig);
 
   await app.listen(port);
   Logger.log(
--- libs/server-core/src/index.ts
+++ libs/server-core/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/server-core.module';
 export * from './lib/log-parser';
 export * from './lib/utilities';
 export * from './lib/dtos';
+export * from './lib/config';
