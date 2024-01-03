81e0209cd8729006ef739acfed3ff4fbc3c0e310
Gunvant Sarpate <gunvantsr@gmail.com>
2024-01-03 17:12:24 +0530

    feat: improved ui for input button

--- apps/app-ui/src/app/components/form.tsx
+++ apps/app-ui/src/app/components/form.tsx
@@ -21,12 +21,24 @@ function FileUploadForm({ onFileUpload }: Props) {
 
   return (
     <div>
-      <h1 className="text-lg">Upload log file</h1>
+      <div className="flex flex-row justify-between">
+        <h1 className="text-xl">Log Parser</h1>
+        <p>
+          by{' '}
+          <a href="https://gunvant.in" className="hover:underline">
+            Gunvant
+          </a>
+        </p>
+      </div>
       <form className="flex flex-col" onSubmit={handleFileUpload}>
-        <input type="file" onChange={handleChange} />
+        <input
+          type="file"
+          onChange={handleChange}
+          className="text-lg mt-5 relative m-0 block w-full min-w-0 flex-auto cursor-pointer rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] font-normal leading-[2.15] text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:cursor-pointer file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none "
+        />
         <button
           type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none  focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white text-lg mt-3 bg-blue-700 hover:bg-blue-800 focus:outline-none  focus:ring-blue-300 font-medium rounded-full px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Upload file
         </button>
